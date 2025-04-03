85fad95b6736949b338191f9fde2fd0dc989dd19
MVladislav <dev@mvladislav.online>
2025-04-03 20:35:41 +0200

    refactor: update getProductIotMap

--- bumper/web/plugins/api/pim/__init__.py
+++ bumper/web/plugins/api/pim/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from typing import Any
 
 
-# EcoVacs Home Product IOT Map - 2023-10-08
+# EcoVacs Home Product IOT Map - 2025-04-03
 # https://portal-ww.ecouser.net/api/pim/product/getProductIotMap
 def get_product_iot_map() -> Any:
     """Get product iot map."""
