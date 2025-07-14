7060eeb7be3faa48e115c4ea7cfcef48ff5e9d51
pikeman20 <hihi9894@gmail.com>
2025-07-14 08:27:15 +0700

    fix: update listen ports in Config class to use environment variables

--- bumper/utils/settings.py
+++ bumper/utils/settings.py
@@ -92,11 +92,11 @@ class Config:
 
     # Ports
     WEB_SERVER_TLS_LISTEN_PORT: int = int(os.environ.get("WEB_SERVER_HTTPS_PORT") or 443)
-    WEB_SERVER_LISTEN_PORT: int = 8007
-    MQTT_LISTEN_PORT: int = 1883
-    MQTT_LISTEN_PORT_TLS: int = 8883
-    XMPP_LISTEN_PORT: int = 1223
-    XMPP_LISTEN_PORT_TLS: int = 5223
+    WEB_SERVER_LISTEN_PORT: int = int(os.environ.get("WEB_SERVER_LISTEN_PORT") or 8007)
+    MQTT_LISTEN_PORT: int = int(os.environ.get("MQTT_LISTEN_PORT") or 1883)
+    MQTT_LISTEN_PORT_TLS: int = int(os.environ.get("MQTT_LISTEN_PORT_TLS") or 8883)
+    XMPP_LISTEN_PORT: int = int(os.environ.get("XMPP_LISTEN_PORT") or 1223)
+    XMPP_LISTEN_PORT_TLS: int = int(os.environ.get("XMPP_LISTEN_PORT_TLS") or 5223)
 
     # Servers
     mqtt_server: "MQTTServer | None" = None
